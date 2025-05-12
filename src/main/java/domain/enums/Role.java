848806e970b66ba99c5f017c516e9c0a7df2ed4b
alwil17 <willialfred24@gmail.com>
2025-05-12 10:42:13 +0000

    Added domain enums

---
+++ src/main/java/domain/enums/Role.java
@@ -0,0 +1,7 @@
+package domain.enums;
+
+public enum Role {
+    ADMIN,
+    STAFF,
+    CLIENT
+}
---
+++ src/main/java/domain/enums/Weekday.java
@@ -0,0 +1,11 @@
+package domain.enums;
+
+public enum Weekday {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
