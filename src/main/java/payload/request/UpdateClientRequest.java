f3aa8b9bf7b0c300ea8d468c6944cf530e0722ae
alwil17 <willialfred24@gmail.com>
2025-05-12 12:43:30 +0000

    Added UpdateClientRequest

---
+++ src/main/java/payload/request/UpdateClientRequest.java
@@ -0,0 +1,15 @@
+package payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import java.util.Set;
+
+public record UpdateClientRequest(
+    String firstName,
+    String lastName,
+    @Email String email,
+    @Pattern(regexp = "^[0-9+\\s-]{6,20}$") String phone,
+    String password, // assume optional update
+    Set<String> roles // must match enum values
+) {}
+
